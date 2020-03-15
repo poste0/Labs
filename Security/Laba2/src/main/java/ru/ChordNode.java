e72c8c6842fe45849e681beb752c870e53ff411e
poste0 <79372009578@yandex.ru>
2020-03-15 19:40:39 +0400

    Initial laba 2

--- Security/Laba2/src/main/java/ru/ChordNode.java
+++ Security/Laba2/src/main/java/ru/ChordNode.java
@@ -1,31 +1,64 @@
 package ru;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ChordNode {
     private Integer id;
 
-    private List<Integer> fingerStarts;
+    private List<FingerTable> fingerStarts;
 
-    private Interval interval;
+    private Integer successor;
 
-    private ChordNode node;
+    private Integer predecessor;
 
-    private ChordNode successor;
-
-    private ChordNode predecessor;
-
-    public ChordNode(int n, int m){
+    public ChordNode(int n, int m, List<Integer> nodes){
         this.id = n;
-        for (int i = 1; i < m; i++) {
-            fingerStarts.add((int) ((n + Math.pow(2, i - 1)) % Math.pow(2, m)));
+
+        fingerStarts = new ArrayList<>();
+        for (int i = 1; i < m + 1; i++) {
+            fingerStarts.add(new FingerTable(n, m, i, nodes));
+        }
+
+        successor = fingerStarts.get(0).getNode();
+
+        for(int i = 0; i < nodes.size(); i++){
+           if(nodes.get(i) >= n){
+               if(i > 0){
+                   predecessor = nodes.get(i - 1);
+                   break;
+               }
+               else {
+                   predecessor = nodes.get(nodes.size() - 1);
+                   break;
+               }
+           }
         }
 
     }
 
-    private class Interval{
-        private int from;
+    @Override
+    public String toString() {
+        return fingerStarts.toString() + " " + successor + " " + predecessor;
+    }
 
-        private int to;
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public List<FingerTable> getFingerStarts(){
+        return fingerStarts;
+    }
+
+    public Integer getSuccessor(){
+        return successor;
+    }
+
+    public Integer getPredecessor(){
+        return predecessor;
     }
 }
--- Security/Laba2/src/main/java/ru/Main.java
+++ Security/Laba2/src/main/java/ru/Main.java
@@ -1,11 +1,11 @@
 package ru;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        testMain();
+        System.out.println("Test ok");
         Scanner scanner = new Scanner(System.in);
 
         final int m;
@@ -22,6 +22,122 @@ public class Main {
             nodes.add(node);
         }
 
+        List<ChordNode> chordNodes = new ArrayList<ChordNode>();
+        for (int i = 0; i < nodes.size(); i++) {
+            chordNodes.add(new ChordNode(nodes.get(i), m, nodes));
+        }
+
+        chordNodes.forEach(chordNode -> {
+            System.out.println("Node " + chordNode.getId());
+            System.out.println(chordNode);
+        });
+    }
+
+    private static void testMain(){
+        List<ChordNode> chordNodes = new ArrayList<>();
+
+        List<Integer> nodes = Arrays.asList(0, 1, 3);
+
+        List<Integer> ids = Arrays.asList(0, 1, 3);
+
+        List<Integer> successors = Arrays.asList(1, 3, 0);
+
+        List<Integer> predecessors = Arrays.asList(3, 0, 1);
+
+        List<List<Integer>> starts = Arrays.asList(Arrays.asList(1, 2, 4),
+                                                    Arrays.asList(2, 3, 5),
+                                                    Arrays.asList(4, 5, 7));
+
+        List<List<List<Integer>>> intervals = Arrays.asList(Arrays.asList(Arrays.asList(1, 2),
+                                                                            Arrays.asList(2, 4),
+                                                                            Arrays.asList(4, 0)),
+                                                            Arrays.asList(Arrays.asList(2, 3),
+                                                                            Arrays.asList(3, 5),
+                                                                            Arrays.asList(5, 1)),
+                                                            Arrays.asList(Arrays.asList(4, 5),
+                                                                            Arrays.asList(5, 7),
+                                                                            Arrays.asList(7, 3)));
+        List<List<Integer>> nodeList = Arrays.asList(Arrays.asList(1, 3, 0),
+                                                        Arrays.asList(3, 3, 0),
+                                                        Arrays.asList(0, 0, 0));
+
+        int m = 3;
+        for (int i = 0; i < nodes.size(); i++) {
+            chordNodes.add(new ChordNode(nodes.get(i), m, nodes));
+        }
+
+        for(int i = 0; i < chordNodes.size(); i++){
+            ChordNode node = chordNodes.get(i);
+            assert node.getId().equals(ids.get(i));
+            assert node.getPredecessor().equals(predecessors.get(i));
+            assert node.getSuccessor().equals(successors.get(i));
+
+            for(int j = 0; j < node.getFingerStarts().size(); j++){
+                FingerTable fingerTable = node.getFingerStarts().get(j);
+
+                assert fingerTable.getNode().equals(nodeList.get(i).get(j));
+                assert fingerTable.getStart().equals(starts.get(i).get(j));
+                assert fingerTable.getFrom().equals(intervals.get(i).get(j).get(0));
+                assert fingerTable.getTo().equals(intervals.get(i).get(j).get(1));
+            }
+
+        }
+
+        assert findSuccessor(2, 0, chordNodes, 3).equals(3);
+        for(int i = 4; i < 8; i++){
+            assert findSuccessor(i, 0, chordNodes, 3).equals(0);
+        }
 
     }
+
+    public static Integer findSuccessor(int id, int startNodeIndex, List<ChordNode> nodes, int m){
+        Integer predecessor = findPredecessor(id, startNodeIndex, nodes, m);
+        Integer result = nodes.stream().filter(e -> {
+            return e.getId().equals(predecessor);
+        }).findFirst().get().getSuccessor();
+        return result;
+    }
+
+    private static Integer findPredecessor(int id, int startNodeIndex, List<ChordNode> nodes, int m){
+        ChordNode node = nodes.get(startNodeIndex);
+        Integer intNode = node.getId();
+        Integer successor = node.getSuccessor();
+        while(notIn(id, intNode, successor, m)){
+            intNode = findClosest(id, intNode, nodes, m);
+            final int finalIntNode = intNode;
+            successor = nodes.stream().filter(e -> {
+                return e.getId().equals(finalIntNode);
+            }).findFirst().get().getSuccessor();
+        }
+        return intNode;
+    }
+
+    private static Integer findClosest(int id, int intNode, List<ChordNode> nodes, int m){
+        ChordNode node = nodes.stream().filter(e -> {
+            return e.getId().equals(intNode);
+        }).findFirst().get();
+        for(int i = nodes.size() - 1; i >= 0; i--){
+
+            if(isIn(node.getFingerStarts().get(i).getNode(), node.getId(), id, m)){
+                return node.getFingerStarts().get(i).getNode();
+            }
+        }
+        return node.getId();
+    }
+
+    private static boolean notIn(int input, int a, int b, int m){
+        if(b < a){
+            b += Math.pow(2, m);
+        }
+        return !(input > a && input <= b);
+    }
+
+    private static boolean isIn(int input, int a, int b, int m){
+        if(b < a){
+            b += Math.pow(2, m);
+            input += Math.pow(2, m);
+        }
+        return input > a && input < b;
+    }
+
 }
