ab4ff14a75fac627e3b04dfbba074d4463718b72
poste0 <79372009578@yandex.ru>
2020-04-20 19:26:22 +0400

    Labs

---
+++ Security/Laba3/src/main/java/ru/Main.java
@@ -0,0 +1,14 @@
+package ru;
+
+import com.dampcake.bencode.Bencode;
+import com.dampcake.bencode.BencodeInputStream;
+import com.dampcake.bencode.Type;
+
+import java.io.*;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+    }
+}
