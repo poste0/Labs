b446112f7957cac79f50aeb0c04a68d77cef9233
poste0 <79372009578@yandex.ru>
2020-09-10 01:45:55 +0400

    a

--- Security/Laba4/src/main/java/ru/controller/MainController.java
+++ Security/Laba4/src/main/java/ru/controller/MainController.java
@@ -1,5 +1,6 @@
 package ru.controller;
 
+import com.sun.org.apache.xalan.internal.xsltc.util.IntegerArray;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,11 +9,10 @@ import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
+import java.nio.file.Files;
 
 @Controller
 public class MainController {
@@ -20,20 +20,17 @@ public class MainController {
     private String filePath;
 
     @RequestMapping(value = "/video", method = RequestMethod.GET, produces = "application/vnd.apple.mpegurl")
-    public @ResponseBody byte[] getVideo(HttpServletResponse response, @RequestParam("name") String name) throws IOException {
-        response.addHeader("Content-Type", "application/vnd.apple.mpegurl");
-        response.addHeader("Accept-Ranges", "bytes");
-        response.addHeader("Access-Control-Allow-Origin", "*");
-        response.addHeader("Access-Control-Expose-Headers", "Content-Length");
-
+    public @ResponseBody void getVideo(HttpServletRequest request, HttpServletResponse response, @RequestParam("name") String name) throws IOException {
         File file = new File(filePath + name);
-        byte[] bytes = FileCopyUtils.copyToByteArray(file);
-
-        return bytes;
+        try {
+            MultipartFileSender.fromPath(file.toPath()).with(request).with(response).serveResource();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @RequestMapping(value = "/{name}", method = RequestMethod.GET, produces = "application/vnd.apple.mpegurl")
-    public @ResponseBody byte[] getPiece(HttpServletResponse response, @PathVariable("name") String name) throws IOException {
-        return getVideo(response, name);
+    public @ResponseBody void getPiece(HttpServletRequest request, HttpServletResponse response, @PathVariable("name") String name) throws IOException {
+        getVideo(request, response, name);
     }
 }
